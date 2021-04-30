8244ec173c241214fec643a1f86c5053ac9c2399
andresgenda <54486269+andresgenda@users.noreply.github.com>
2021-04-30 16:07:57 -0700

    Added Reserve Functionality

--- server.js
+++ server.js
@@ -7,8 +7,37 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var tablesInfo = [];
+var waitlistInfo = [];
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+
 app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
+    res.sendFile(path.join(__dirname, "/reserve.html"));
+});
+
+app.get("/tables", function(req,res){
+    res.sendFile(path.join(__dirname, "/tables.html"));
+});
+
+app.get("/api/tables", function(req,res){
+    res.json(tablesInfo);
+});
+
+app.get("/api/waitlist", function(req,res){
+    res.json(waitlistInfo);
+})
+
+app.post("/api/tables", function(req,res){
+    if(tablesInfo.length > 5){
+        waitlistInfo.push(req.body);
+        res.json(false);
+    }else{
+        tablesInfo.push(req.body);
+        res.json(true);
+    }
 });
 
 app.listen(PORT, function() {
